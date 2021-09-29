260ef3182e30af54c0e4042d2208b51afb999b1c
harshitn2002 <84696979+harshitn2002@users.noreply.github.com>
2021-09-29 09:08:38 +0530

    Delete PRACTICAL 2/TASK 3 directory

--- PRACTICAL 2/TASK 3/task3a.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	char a;
-	cin>>a;
-	cout<<a<<"\n";
-	return 0;
-}
--- PRACTICAL 2/TASK 3/task3b.cpp
+++
@@ -1,9 +0,0 @@
-#include <iostream>
-using namespace std;
-int main()
-{
-	char a[50];
-	cin.get(a,50);
-	cout<<a<<"\n";
-	return 0;
-}
