de025933698eeccf665da1ba7095eed026b34fb3
harshitn2002 <84696979+harshitn2002@users.noreply.github.com>
2021-10-07 13:50:03 +0530

    Add files via upload

---
+++ PRACTICAL 6/TASK 3/code1_reversefunc.cpp
@@ -0,0 +1,13 @@
+#include <bits/stdc++.h>
+using namespace std;
+int main()
+{
+    string str;
+    
+    getline(cin,str);
+ 
+    reverse(str.begin(), str.end());
+ 
+    cout << str << "\n";
+    return 0;
+}
