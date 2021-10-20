4db0ee07cd48eb78ce6d5e53c03105c7c761cc1f
harshitn2002 <84696979+harshitn2002@users.noreply.github.com>
2021-10-20 13:38:59 +0530

    Add files via upload

---
+++ PRACTICAL 8/TASK 3/task3a.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+  int arr[] = {10,20,30};
+  cout << *arr << endl;
+  arr++;
+  cout << *arr << endl;
+  return 0;
+}
+
---
+++ PRACTICAL 8/TASK 3/task3b.cpp
@@ -0,0 +1,10 @@
+#include<iostream>
+#include<bits/stdc++.h>
+using namespace std; 
+int main()
+{
+  int arr[] = {10,20,30};
+  cout << *arr << endl;
+  cout << *(arr+1) << endl;
+  return 0;
+}
